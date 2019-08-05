2d36bc6613ae4f41b3e68bfa71d1c197ade99d30
Sasha Belova <s2288b@gmail.com>
2019-08-05 17:41:11 -0400

    type classes add

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -12,13 +12,37 @@ class User extends Component {
         this.props.status(this.props.id, activeStatus);
     };
 
+    renderType() {
+        let type;
+
+        if (this.props.type === 'Employee') {
+            type =
+                <td className="table-content_type type-em"><div>EM</div></td>
+        } else if (this.props.type === 'Stakeholder') {
+            type =
+                <td className="table-content_type type-sh"><div>SH</div></td>
+        }
+        else if (this.props.type === 'Supervisor') {
+            type =
+                <td className="table-content_type type-sv"><div>SV</div></td>
+        }
+        else if (this.props.type === 'Guest') {
+            type =
+                <td className="table-content_type type-gt"><div>GT</div></td>
+        } else {
+            type =
+                <td className="table-content_type type-un"><div>UN</div></td>
+        }
+
+        return type;
+    }
 
 
     render() {
         return (
             <tr className="table-content">
                 <td><input type="checkbox"/></td>
-                <td>{this.props.type}</td>
+                {this.renderType()}
                 <td className="table-content_name">{this.props.name}</td>
                 <td>{this.props.email}</td>
                 <td>{this.props.phone}</td>
